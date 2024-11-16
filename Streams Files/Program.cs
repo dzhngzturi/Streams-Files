a2110ac199a7477e2e4d091a01ea6dd01f7019d6
dzhengiz.turhan@outlook.com <dzhengiz.turhan@outlook.com>
2024-11-16 12:09:37 +0200

    First Exercise

--- Streams Files/Program.cs
+++ Streams Files/Program.cs
@@ -2,8 +2,22 @@
 {
     private static void Main(string[] args)
     {
-       
+        string filePath = @"C:\Users\dzhengiz\source\repos\Sirma Academy\input.txt";
 
+        using(StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] words = line.Split(' ');
+
+                foreach(string word in words)
+                {
+                    Console.WriteLine(word.Length);
+                }
+            }
+        }
 
 
     }
