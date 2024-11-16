7aeb69310c83edbb0f57eb6dcf1578898159c83f
dzhengiz.turhan@outlook.com <dzhengiz.turhan@outlook.com>
2024-11-16 12:21:56 +0200

    Second Exercise

--- Streams Files/Program.cs
+++ Streams Files/Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main(string[] args)
     {
+        /*===================1=============
         string filePath = @"C:\Users\dzhengiz\source\repos\Sirma Academy\input.txt";
 
         using(StreamReader reader = new StreamReader(filePath))
@@ -18,7 +19,35 @@
                 }
             }
         }
+        ==================================*/
 
+        string filePath = @"C:\Users\dzhengiz\source\repos\Sirma Academy\input.txt";
+
+        Dictionary<char, int> charFrequency = new Dictionary<char, int>();
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            int currentChar;
+
+            while ((currentChar = reader.Read()) != -1)
+            {
+                char ch = (char)currentChar;
+
+                if (charFrequency.ContainsKey(ch))
+                {
+                    charFrequency[ch]++;
+                }
+                else
+                {
+                    charFrequency[ch] = 1;
+                }
+            }
+        }
+
+        foreach (var item in charFrequency)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
 
     }
 }
